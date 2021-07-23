d3d896a8ea83774e35da5d13c460f4ba3a2eb288
Boran Seckin <boran@apostonews.com>
2021-07-23 21:28:37 +0300

    version sukufe

--- app/src/main/java/com/example/boo/MainActivity.kt
+++ app/src/main/java/com/example/boo/MainActivity.kt
@@ -2,39 +2,29 @@ package com.example.boo
 
 import android.hardware.usb.UsbManager
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hoho.android.usbserial.driver.UsbSerialPort
 import com.hoho.android.usbserial.driver.UsbSerialProber
 
-
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var textField: TextView
-    private lateinit var button: Button
+    private lateinit var refreshButton: Button
     private lateinit var ledButton: Button
 
     private lateinit var port: UsbSerialPort
 
     private var state = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        textField = findViewById(R.id.textField)
-        button = findViewById(R.id.button)
-        ledButton = findViewById(R.id.ledButton)
-
-        button.setOnClickListener(this)
-        ledButton.setOnClickListener(this)
-
+    private fun connect() {
         val manager = getSystemService(USB_SERVICE) as UsbManager
         val availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager)
         if (availableDrivers.isEmpty()) {
             state = false
+            Toast.makeText(applicationContext,"No driver found!", Toast.LENGTH_SHORT).show()
             return
         }
         state = true
@@ -43,18 +33,55 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val driver = availableDrivers[0]
         val connection = manager.openDevice(driver.device) ?: return
 
-        port = driver.ports[0] // Most devices have just one port (port 0)
-        port.open(connection)
-        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+        if (driver.ports.size < 1) {
+            Toast.makeText(applicationContext,"No device found!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        try {
+            port = driver.ports[0] // Most devices have just one port (port 0)
+            port.open(connection)
+            port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show()
+            state = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textField = findViewById(R.id.textField)
+        refreshButton = findViewById(R.id.refreshButton)
+        ledButton = findViewById(R.id.ledButton)
+
+        refreshButton.setOnClickListener(this)
+        ledButton.setOnClickListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        connect()
+        updateUI()
+    }
+
+    private fun updateUI() {
+        textField.text = if (state) "yes driver" else "no driver"
+        ledButton.isEnabled = state
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.button -> {
-                textField.text = if (state) "yes driver" else "no driver"
+            R.id.refreshButton -> {
+                connect()
+                updateUI()
             }
             R.id.ledButton -> {
-                port.write("led".toByteArray(), 500)
+                try {
+                    port.write("led".toByteArray(), 500)
+                } catch (e: Exception) {
+                    Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
